db676bda494b673abb3c14c991e5c84508198d7d
RajkovicNevena <nevenarajkovic2402@gmail.com>
2024-12-16 15:57:47 +0100

    Domaci sa prvog predavanja

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", function(){
-    echo "123";
+/*
+    Domaci:
+    1. glavna stranica (welcome.blade.php)
+    2.about
+    3.shop
+    4.contact
+*/
+
+Route::view("/", "welcome");
+
+Route::get("/about", function () {
+    return view("about");
+});
+
+
+Route::get("/shop", function (){
+    return view("shop");
+});
+
+Route::get("/contact", function (){
+    return view("contact");
 });
